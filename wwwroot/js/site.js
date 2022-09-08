8077b15c3ae3b943ee31b00b81b3af5c0f4bd6ce
Egor Geberleyn <egeberleyn@gmail.com>
2022-09-09 01:43:58 +0300

    add profile views, fix bugs

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -23,3 +23,15 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
+
+
+$('body').on('click', '.password-control', function () {
+	if ($('#password-input').attr('type') == 'password') {
+		$(this).addClass('view');
+		$('#password-input').attr('type', 'text');
+	} else {
+		$(this).removeClass('view');
+		$('#password-input').attr('type', 'password');
+	}
+	return false;
+});
