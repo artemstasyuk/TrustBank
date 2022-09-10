a3e726cdb79c5f3cddae2400b50855ebc5f40b6d
Egor Geberleyn <egeberleyn@gmail.com>
2022-09-10 13:01:01 +0300

    add auth validation, login validation, order validation, design verify email views

--- ViewModels/CheckoutViewModel.cs
+++ ViewModels/CheckoutViewModel.cs
@@ -4,13 +4,19 @@ namespace BankApplication.ViewModels;
 
 public class CheckoutViewModel
 {
+    [StringLength(30, ErrorMessage = "Name must match the format")]
+    [Required(ErrorMessage = "Enter your name")]
     public string Name { get; set; }
-    
+
+    [StringLength(50, ErrorMessage = "Surname must match the format")]
+    [Required(ErrorMessage = "Enter your surname")]
     public string Surname { get; set; }
-    
-    [DataType(DataType.EmailAddress)]
+
+    [EmailAddress(ErrorMessage = "Email must match the format")]
+    [Required(ErrorMessage = "Enter your email")]
     public string Email { get; set; }
-    
+
+    [Required(ErrorMessage = "Please enter your adress")]
     public string Adress { get; set; }
 
 }
