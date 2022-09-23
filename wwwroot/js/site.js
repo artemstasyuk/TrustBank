2e31a0cc57e345f0c4f91816cb9b07ffdc042a47
egeberleyn <egeberleyn@gmail.com>
2022-09-23 12:02:36 +0300

    fixed bugs

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,28 +1,21 @@
-﻿// Получить модальный
-var modal = document.getElementById("myModal");
-
-// Получить кнопку, которая открывает модальный
+﻿var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 
 var avatar_btn = document.getElementById("avatarBtn");
-
-// Получить элемент <span>, который закрывает модальный
 var span = document.getElementsByClassName("close")[0];
 
-// Когда пользователь нажимает на кнопку, откройте модальный
 btn.onclick = function () {
     modal.style.display = "block";
 }
+
 avatar_btn.onclick = function () {
     modal.style.display = "block";
 }
 
-// Когда пользователь нажимает на <span> (x), закройте модальное окно
 span.onclick = function () {
     modal.style.display = "none";
 }
 
-// Когда пользователь щелкает в любом месте за пределами модального, закройте его
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
